39acfc807c4ba9e45296efa15a2f730fee80f710
Michael Tillman <mnamllit@gmail.com>
2022-07-21 14:47:27 -0500

    removed default styling segmented navbar into a component.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+
+
+}
+</script>
+
+<style lang="sass">
+
+</style>
