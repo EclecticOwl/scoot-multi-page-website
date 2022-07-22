4155123b477c91d26c73eff8b20d1fdfdbda47b4
Michael Tillman <mnamllit@gmail.com>
2022-07-22 13:39:56 -0500

    added faq section to the about page.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -70,11 +70,119 @@
 		</div>
 		</div>
 	</section>
+	<section>
+		<h1>FAQs</h1>
+
+		<div class="faq-container">
+			<h2>How it works</h2>
+			<div class="faq-1">
+
+				<div class="faq-item">
+				<div class="faq-title" @click="show1 = !show1">
+					<span>How do I download the app?</span>
+					<img v-if="show1" src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+					<img class="invert" v-else src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+			
+				</div>
+				<Transition name="fade">
+					<p v-if="show1">To download the Scoot app, you can search “Scoot” in both the App and Google Play stores. 
+	An even simpler way to do it would be to click the relevant link at the bottom of this 
+	page and you'll be re-directed to the correct page.</p>
+				</Transition>
+				</div>
+
+				<div class="faq-item">
+				<div class="faq-title" @click="show = !show">
+					<span>Can I find nearby Scoots?</span>
+					<img v-if="show" src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+					<img class="invert" v-else src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+			
+				</div>
+				<Transition name="fade">
+					<p v-if="show">Definitely! Simply open up the app and allow us to find your location while using it. We'll 
+  show you all of the closest Scoots and some extra useful information.</p>
+				</Transition>
+				</div>
+
+				<div class="faq-item">
+				<div class="faq-title" @click="show2 = !show2">
+					<span>Do I need a license to ride?</span>
+					<img v-if="show2" src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+					<img class="invert" v-else src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+			
+				</div>
+				<Transition name="fade">
+					<p v-if="show2">Yup! We provide information inside the app regarding local laws and the license you need to be 
+  able to ride our Scoots.</p>
+				</Transition>
+				</div>
+			</div>
+		</div>
+
+		<div class="faq-container">
+			<h2>Safe driving</h2>
+			<div class="faq-1">
+
+				<div class="faq-item">
+				<div class="faq-title" @click="show3 = !show3">
+					<span>Should I wear a helmet?</span>
+					<img v-if="show3" src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+					<img class="invert" v-else src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+			
+				</div>
+				<Transition name="fade">
+					<p v-if="show3">Yes, please do! All cities have different laws. But we strongly strongly strongly recommend 
+  always wearing a helmet regardless of the local laws. We like you and we want you to be as 
+  safe as possible while Scooting.</p>
+				</Transition>
+				</div>
+
+				<div class="faq-item">
+				<div class="faq-title" @click="show4 = !show4">
+					<span>How about the rules & regulations?</span>
+					<img v-if="show4" src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+					<img class="invert" v-else src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+			
+				</div>
+				<Transition name="fade">
+					<p v-if="show4">Now is not the time to be a rule breaker. Be sure you're complying with all local laws and 
+  regulations. Also, just be a good human being. Be sure not to park your Scoot where it can 
+  block access to buildings or get in people's way.</p>
+				</Transition>
+				</div>
+
+				<div class="faq-item">
+				<div class="faq-title" @click="show5 = !show5">
+					<span>What if I damage my Scoot?</span>
+					<img v-if="show5" src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+					<img class="invert" v-else src="@/assets/icons/chevron.svg?inline" alt="chevron denoting whether the item is closed">
+			
+				</div>
+				<Transition name="fade">
+					<p v-if="show5">Be sure to read our terms and conditions carefully. Not the most fun job we know but we make 
+  it as clear as possible. There's an option to add insurance for each trip, or you can sign 
+  up for annual insurance if you're a regular Scooter.</p>
+				</Transition>
+				</div>
+			</div>
+		</div>
+
+	</section>
 </template>
 
 <script>
 export default {
 	name: 'AboutView',
+	data()  {
+		return {
+			show: false,
+			show1: false,
+			show2: false,
+			show3: false,
+			show4: false,
+			show5: false,
+		}
+	}
 }
 </script>
 
@@ -93,6 +201,8 @@ header
 	padding-left: 5em
 	color: white
 	font-size: 1.5em
+main
+	overflow: hidden
 .main-col-1, .main-col-2
 	margin-top: 6em
 	display: flex
@@ -163,7 +273,7 @@ section
 	flex-direction: column
 	align-items: center
 	gap: 6em
-	
+	padding: 0 2em
 	h1
 		font-size: 1.7em
 .value-container
@@ -197,9 +307,45 @@ section
 	display: flex
 	flex-direction: column
 	align-items: center
-
+	p
+		text-align: center
+.fade-enter-active, .fade-leave-active 
+  transition: opacity .5s ease
+.fade-enter-from, .fade-leave-to 
+  opacity: 0
+.faq-item
+	margin-top: .5em
+	display: flex
+	flex-direction: column
+	justify-content: center
+	align-items: center
+	gap: 1em
+	width: 500px
+	background-color: #F2F4F9
+	padding: .75em 3em
+.faq-title
+	width: 100%
+	display: flex
+	flex-direction: row
+	justify-content: space-between
+	align-items: center
+	img
+		height: 15px
+		width: 15px
+.faq-container
+	width: 100%
+	display: flex
+	justify-content: space-between
+	padding: 0 10em
+.invert
+	transform: rotate(180deg)
 
 @media (max-width: 800px)
+	.faq-container
+		flex-direction: column
+		justify-content: center
+		align-items: center
+		gap: 1em
 	.main-col-1, .main-col-2
 		flex-direction: column
 		align-items: center
@@ -207,6 +353,8 @@ section
 		flex-direction: column
 
 @media (max-width: 500px)
+	.faq-item
+		width: 350px
 	.main-col-1::after, .main-col-2::after
 		display: none
 	.main-col-1::before
